fed5d014ec4d2e5ca5b62648e01cfd5faf678616
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-07-05 10:38:16 +0700

    update: routing

--- src/components/seeMore/KeyboardInfo.js
+++ src/components/seeMore/KeyboardInfo.js
@@ -5,92 +5,92 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { getKeyboardData, setKeyboardInfo } from '../../state/reducer/action';
 
 function KeyboardInfo() {
-    const params = useParams();
-    const state = useSelector((state) => state.keyboard);
-    const redirect = useNavigate()
-    const dispatch = useDispatch();
-    const { keyboardInfo, keyboardData } = state
-    const { keyboardId } = params
+  const params = useParams();
+  const state = useSelector((state) => state.keyboard);
+  const redirect = useNavigate()
+  const dispatch = useDispatch();
+  const { keyboardInfo, keyboardData } = state
+  const { keyboardId } = params
 
-    const assignKbInfo = () => {
-        keyboardData.map((keyboard) => {
-            if (keyboard.id == keyboardId) {
-                dispatch(
-                    setKeyboardInfo(keyboard)
-                )
-            }
-        })
+  const assignKbInfo = () => {
+    keyboardData.map((keyboard) => {
+      if (keyboard.id == keyboardId) {
+        dispatch(
+          setKeyboardInfo(keyboard)
+        )
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (keyboardData.length <= 0) {
+      dispatch(getKeyboardData())
     }
+    else {
+      assignKbInfo()
+    }
+  }, [keyboardData])
 
-    useEffect(() => {
-        if (keyboardData.length <= 0){
-            dispatch(getKeyboardData())
-        }
-        else{
-            assignKbInfo()
-        }
-    }, [keyboardData])
+  return (
+    <div className='KeyboardInfo OutletCommon'>
+      <button
+        onClick={() => redirect('/keyboards')}
+      >
+        Back
+      </button>
+      <div className="title">
+        {keyboardInfo.name}
+      </div>
 
-    return (
-        <div className='KeyboardInfo OutletCommon'>
-            <button
-                onClick={() => redirect('/keyboard')}
-                >
-                Back
-            </button>
-            <div className="title">
-                {keyboardInfo.name}
-            </div>
+      <div className="info">
+        <img
+          src={keyboardInfo.imgUrls?.[0]} alt="img here"
+          className="img" />
 
-            <div className="info">
-                <img 
-                    src={keyboardInfo.imgUrls?.[0]} alt="img here"
-                    className="img" />
-
-                <div className="info-title">
-                    Start Date:
-                </div>
-
-                <div>
-                    {keyboardInfo.startDate?.getDate() + '/'
-                      + (keyboardInfo.startDate?.getMonth()+1) + '/'
-                      + keyboardInfo.startDate?.getFullYear()}
-                </div>
-
-                <div className="info-">
-                    End Date:
-                </div>
-
-                <div>
-                    {keyboardInfo.endDate?.getDate() + '/'
-                      + (keyboardInfo.endDate?.getMonth()+1) + '/'
-                      + keyboardInfo.endDate?.getFullYear()}
-                </div>
-
-                <div className="info-title">
-                    Prices
-                </div>
-
-                <div className="price">${keyboardInfo.basePrice}</div>
-
-                <div className="info-title">
-                    Vendors
-                </div>
-
-                <div>
-                    {keyboardInfo.vendors?.map((vendor, index) => (
-                        <div key={index}>
-                            <div>{vendor.region}:</div>
-                            <div>{vendor.url}</div>
-                        </div>
-                    ))}
-                </div>
-
-                {/* //TODO: check if there is a geekhack link */}
-
-            </div>
+        <div className="info-title">
+          Start Date:
         </div>
-    )
+
+        <div>
+          {keyboardInfo.startDate?.getDate() + '/'
+            + (keyboardInfo.startDate?.getMonth() + 1) + '/'
+            + keyboardInfo.startDate?.getFullYear()}
+        </div>
+
+        <div className="info-">
+          End Date:
+        </div>
+
+        <div>
+          {keyboardInfo.endDate?.getDate() + '/'
+            + (keyboardInfo.endDate?.getMonth() + 1) + '/'
+            + keyboardInfo.endDate?.getFullYear()}
+        </div>
+
+        <div className="info-title">
+          Prices
+        </div>
+
+        <div className="price">${keyboardInfo.basePrice}</div>
+
+        <div className="info-title">
+          Vendors
+        </div>
+
+        <div>
+          {keyboardInfo.vendors?.map((vendor, index) => (
+            <div key={index}>
+              <div>{vendor.region}:</div>
+              <div>{vendor.url}</div>
+            </div>
+          ))}
+        </div>
+
+        {/* //TODO: check if there is a geekhack link */}
+
+      </div>
+    </div>
+  )
 }
 
 export default KeyboardInfo
