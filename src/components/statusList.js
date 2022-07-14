775941ec222b64fe2bd2de416538e3427d1e1179
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-07-14 10:29:26 +0700

    relocation

--- src/components/statusList.js
+++
@@ -1,7 +0,0 @@
-export const statusList = [
-    "live",
-    "in stock",
-    "upcoming",
-    "IC",
-    "ended"
-]
--- src/components/typeList.js
+++
@@ -1,5 +0,0 @@
-export const typeList = [
-    "keyboards",
-    "keycaps",
-    "switches"
-]
